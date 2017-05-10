02ffd4377244e490c1a0bb6ba8c15721ee3f697c
Bartosz Budak <bartoszbudak@Bartoszs-MacBook-Pro.local>
2017-05-09 21:16:11 -0400

    minor text fixes

--- src/Projects.js
+++ src/Projects.js
@@ -13,14 +13,14 @@ export default class Projects extends Component {
         <img id="pic" src={reactLogo}></img>
         </div>
         <div>
-        <h2>As I begin my career as a web developer, I wanted to perfect my fron-end development skills, and then further
-        purse fullstack knowledge. Using the theory that I learned at Rutgers University, I am confident that I can create
+        <h2>As I begin my career as a web developer, I wanted to perfect my front-end development skills, and then further
+        pursue fullstack. Using the theory that I learned at Rutgers University, I am confident that I can create
         meaningful yet complex website for users of all ages.</h2>
         </div>
         <div id='test'>
         <img id="wk" src={wk}></img>
           <h2>As someone who studied Japanese in University, I want to react an React Native application that facilitates the
-          study of the language through the use of the WaniKani web service. It is a project that involves fullstack so it
+          study of the language through the use of the WaniKani web service. It is a fullstack project so it
           will be a great learning curve for me and will propel my skills in web development.</h2>
         </div>
       </div>
